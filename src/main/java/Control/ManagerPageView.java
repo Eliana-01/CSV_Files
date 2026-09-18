d69249918e7d7fa7e7f555a0183920d74b8e7c75
Eliana-dev <attsombrero@gmail.com>
2026-09-17 19:09:25 -0500

    Initial Commit

---
+++ src/main/java/Control/ManagerPageView.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Control;
+
+import Model.WebPages; //Se importa o no el nombre del arraylist??
+import java.util.List;
+/**
+ *
+ * @author EstudianteLIS
+ */
+public interface ManagerPageView {
+    List<WebPages> pagesView();
+}
---
+++ src/main/java/Model/WebPages.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+/**
+ *
+ * @author EstudianteLIS
+ */
+public class WebPages {
+    private int index;
+    private String pageID;
+    private String url;
+    private String pageTitle;
+    private String pageText;
+    private String extractionTask;
+    
+    public WebPages(){
+        
+    }
+
+    public WebPages(int index, String pageID, String url, String pageTitle, String pageText, String extractionTask) {
+        this.index = index;
+        this.pageID = pageID;
+        this.url = url;
+        this.pageTitle = pageTitle;
+        this.pageText = pageText;
+        this.extractionTask = extractionTask;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public String getPageID() {
+        return pageID;
+    }
+
+    public void setPageID(String pageID) {
+        this.pageID = pageID;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getPageTitle() {
+        return pageTitle;
+    }
+
+    public void setPageTitle(String pageTitle) {
+        this.pageTitle = pageTitle;
+    }
+
+    public String getPageText() {
+        return pageText;
+    }
+
+    public void setPageText(String pageText) {
+        this.pageText = pageText;
+    }
+
+    public String getExtractionTask() {
+        return extractionTask;
+    }
+
+    public void setExtractionTask(String extractionTask) {
+        this.extractionTask = extractionTask;
+    }
+}
